6b226ace0663bea68abcbefe945411734988c97a
Andres Lamont <alamont@gmail.com>
2014-09-09 11:03:22 +0200

    Read zip files for bulk_impoty

--- bulk_import.js
+++ bulk_import.js
@@ -3,6 +3,7 @@ elasticsearch = require('elasticsearch');
 var log4js = require('log4js');
 Util = require('util');
 require('./onx.js');
+var AdmZip = require('adm-zip');
 
 // var serverOptions = {
 //     host: 'youbedo:foldyourbooks@09cef314ab007c8a000.qbox.io:80',
@@ -95,35 +96,44 @@ mapping = {
 client.indices.create({index: "books"})
   .then(function (body) { client.indices.putMapping({index: "books", type: "print_book", body: mapping}) });
 
-files = fs.readdirSync('onx');
+files = fs.readdirSync('/Users/Andres/SkyDrive/Orikami/Youbedo/ONIX');
+files = files.filter(function(f){return f.search(/.zip$/i) >= 0});
+console.log(files)
 i = 0;
 
 bulk = function(files,i, callback){
 
-  readOnx("onx/" + files[i], function(data){
+  onx_zip = new AdmZip("/Users/Andres/SkyDrive/Orikami/Youbedo/ONIX/" + files[i]);
 
-    books = data.ONIXMessage.Product;
-    commands = [];
-    books.forEach(function (item) {
-      recordReference = item.RecordReference;
-      commands.push({ index : { _index :"books", _type : "print_book", _id: recordReference } });
-      commands.push(item);
-    })
+  onx_zip.getEntries().forEach(function(onx_xml_file) {
 
-    client.bulk({body: commands}, function(err,resp){
-      if (err) {
-        console.error('WTF!!!');
-      } else {
-        callback(files[i] + " " + (i + 1) + "/" + files.length);
-        if ((i+1) < files.length){
-          bulk(files,i+1, function(res){
-            console.info(res);
-          });
-        }else{
-          console.info("Done!");
-          // process.exit();
+    onx_xml = onx_xml_file.getData().toString('utf8');
+
+    parseOnx(onx_xml, function(data){
+
+      books = data.ONIXMessage.Product;
+      commands = [];
+      books.forEach(function (item) {
+        recordReference = item.RecordReference;
+        commands.push({ index : { _index :"books", _type : "print_book", _id: recordReference } });
+        commands.push(item);
+      })
+
+      client.bulk({body: commands}, function(err,resp){
+        if (err) {
+          console.error('WTF!!!');
+        } else {
+          callback(files[i] + " " + (i + 1) + "/" + files.length);
+          if ((i+1) < files.length){
+            bulk(files,i+1, function(res){
+              console.info(res);
+            });
+          }else{
+            console.info("Done!");
+            // process.exit();
+          }
         }
-      }
+      });
     });
   });
 
